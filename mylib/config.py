b80fc5daa0096ffb4e8a6f198da398df0d19d930
Saranishkumar <59037331+saranishkumar@users.noreply.github.com>
2021-05-17 15:22:44 +0530

    Add files via upload

---
+++ mylib/config.py
@@ -0,0 +1,17 @@
+
+MAIL = 'saranishkumar11@gmail.com'
+# Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
+url = 'rtsp://tapoadmin:Saranish%40123@192.168.0.160/stream1'
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = False
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 4
+Thread=False
+Scheduler=False
+# Simple log to log the counting data
+Log = True
+# Auto stop the software after certain a time/hours
+Timer = False
+#===============================================================================
+#===============================================================================
